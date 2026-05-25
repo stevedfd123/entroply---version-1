aa521b2d1e604b57ae8dd7f974e434ba15907d69
stevedfd123 <stevedfd123@gmail.com>
2026-05-25 21:54:23 +0530

    Change base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/remix-entropy-flow-visualizer/',
+  base: '/entroply---version-1/',
 
   plugins: [react(), tailwindcss()],
 
